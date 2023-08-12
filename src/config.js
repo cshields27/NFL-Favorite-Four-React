6bbdaf8d44ea13ffcab1eb23a2cb6e4bf93568a4
cshields27 <cshield6@nd.edu>
2023-08-11 23:26:10 -0400

    Fully working frontend except Google login due to no https

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // set with NODE_ENV=production npm run build, ex.
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://your-production-api-url.com' : 'http://localhost:8000';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://06nq0so38b.execute-api.us-east-1.amazonaws.com/dev' : 'http://localhost:8000';
+//const API_URL = process.env.NODE_ENV === 'production' ? 'https://your-production-api-url.com' : 'http://192.168.0.14:8000';
 
 export default {
   API_URL,
