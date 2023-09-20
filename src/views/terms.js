e06b916e3dbc8fe87d2efaf67684315c4177953d
cshields27 <cshield6@nd.edu>
2023-09-20 14:00:29 -0400

    update

--- src/views/terms.js
+++ src/views/terms.js
@@ -21,34 +21,38 @@ const Terms = () => {
             <br/><br/>
 
             <strong>3. User's Responsibilities:</strong><br/>
-            All users are expected to engage with the platform in good faith. Misuse, including but not limited to cheating or attempting to manipulate results, will result in account suspension or termination.
+            All users are expected to engage with the platform in good faith. Misuse, including but not limited to cheating or gambling, will result in account suspension or termination.
             <br/><br/>
 
-            <strong>4. Termination and Account Cancellation:</strong><br/>
+            <strong>4. Prohibition on Gambling</strong><br/>
+            Favorite Four is intended solely for entertainment purposes and may not be used in connection with any form of gambling or wagering. Users are strictly prohibited from using the platform for gambling or any illegal activities.
+            <br/><br/>
+
+            <strong>5. Termination and Account Cancellation:</strong><br/>
             Favorite Four reserves the right to terminate or suspend any account without prior notice if a violation of the Terms of Service is suspected.
             <br/><br/>
 
-            <strong>5. Changes to the Terms:</strong><br/>
+            <strong>6. Changes to the Terms:</strong><br/>
             We may update these Terms of Service from time to time. It is the responsibility of the user to review any changes made to these terms.
             <br/><br/>
 
-            <strong>6. Governing Law and Jurisdiction:</strong><br/>
+            <strong>7. Governing Law and Jurisdiction:</strong><br/>
             All disputes related to these terms will be resolved under the laws and jurisdiction of the location where the website's primary operations are based.
             <br/><br/>
 
-            <strong>7. No Waiver:</strong><br/>
+            <strong>8. No Waiver:</strong><br/>
             Failure by Favorite Four to enforce any provision of these Terms of Service will not be considered a waiver of such provision.
             <br/><br/>
 
-            <strong>8. Limitation of Liability:</strong><br/>
+            <strong>9. Limitation of Liability:</strong><br/>
             Favorite Four will not be liable for any indirect, incidental, or consequential damages resulting from the use or inability to use the service, or from any communications or interactions with other users.
             <br/><br/>
 
-            <strong>9. Entire Agreement:</strong><br/>
+            <strong>10. Entire Agreement:</strong><br/>
             These Terms of Service constitute the entire agreement between Favorite Four and you concerning the subject matter hereof.
             <br/><br/>
 
-            <strong>10. Contact Information:</strong><br/>
+            <strong>11. Contact Information:</strong><br/>
             For any queries regarding these Terms of Service or any other issues related to Favorite Four, please reach out to our contact page.
           </p>
         </div>
